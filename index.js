8ab4a894c5f5a3573110a3c2ab738f6d63243127
SergeyTsay <sergeytsay1805@gmail.com>
2023-07-24 14:13:41 +0300

    Added frontend

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ morgan.token('req-body', (req) => {
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(
-    morgan(':method :url :status :res[content-length] - :response-time ms :req-body')
-  );
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+app.use(express.static('build'))
 let persons = [
     { 
       "id": 1,
